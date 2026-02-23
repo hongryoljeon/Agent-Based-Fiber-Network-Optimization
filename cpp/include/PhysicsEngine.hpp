ed59b7a197113f1345f3076f78abfc886cfbaac1
hongryoljeon <hongryol.jeon@gmail.com>
2026-02-22 20:40:16 -0600

    Update PhysicsEngine.hpp

--- cpp/include/PhysicsEngine.hpp
+++ cpp/include/PhysicsEngine.hpp
@@ -1,1 +1,27 @@
 
+#ifndef PHYSICS_ENGINE_H
+#define PHYSICS_ENGINE_H
+
+#include <Eigen/Dense>
+#include <vector>
+
+class PhysicsEngine {
+public:
+    // Python의 energy_numba와 동일
+    static double calculate_energy(
+        const Eigen::VectorXd& move_point_flat, 
+        const Eigen::MatrixXd& bound_point, 
+        const Eigen::MatrixXd& e_matrix, 
+        double C_val
+    );
+
+    // Python의 gradient_numba와 동일
+    static Eigen::VectorXd calculate_gradient(
+        const Eigen::VectorXd& move_point_flat, 
+        const Eigen::MatrixXd& bound_point, 
+        const Eigen::MatrixXd& e_matrix, 
+        double C_val
+    );
+};
+
+#endif
